12e934a5377300269c1417c80312a2e1f2ef013d
“Jovani <jyhskaiser@gmail.com>
2025-12-03 17:59:41 -0600

    Se agrega la configuracion CORS para pruebas en local

---
+++ src/main/java/jyhs/s1torneos/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package jyhs.s1torneos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Ejemplo de configuración CORS global en Spring Boot
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("http://localhost:5173") // <-- TU ORIGEN
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
